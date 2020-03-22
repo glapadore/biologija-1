96dca51fac3dc483276e100b28947e934be1859d
glapadore <kristers.cipkins@grobinasskola.lv>
2020-03-22 16:42:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('virslapa.html')
+
+
+@app.route('/saturs')
+def saturs():
+    return render_template('saturs.html')
+
+@app.route('/bilde')
+def bilde():
+    return render_template('bilde.html')
+
+app.run(host='0.0.0.0', port=8020)
