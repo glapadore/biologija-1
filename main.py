0c89adc988dbb3110b1a4a83d24b82fdbf4584fc
glapadore <kristers.cipkins@grobinasskola.lv>
2020-05-10 02:04:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request
 import pusher
+from flask import Flask, render_template, jsonify, request
+from flask_sqlalchemy import SQLAlchemy 
+
 app = Flask(__name__)
 
 pusher_client = pusher.Pusher(
